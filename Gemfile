d048d596ddc25aa3b2b2e4292a78d0c649dea301
Arun-Balasubramanian-1 <arunb97eee@gmail.com>
2021-01-23 13:06:05 +0530

    User model, validation, bcrypt and hirb table view

--- Gemfile
+++ Gemfile
@@ -32,7 +32,10 @@ gem 'jquery-rails'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
+# Table view in rails console
+# Hirb.enable and Hirb.disable in console
+gem 'hirb'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Arun_1", password: "password_1")
+User.create(username: "Arun_2", password: "password_2")
+User.create(username: "Arun_3", password: "password_3")
+User.create(username: "Arun_4", password: "password_4")
+User.create(username: "Arun_5", password: "password_5")
+User.create(username: "Arun_6", password: "password_6")
